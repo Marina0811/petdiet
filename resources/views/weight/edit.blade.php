703a3645df1532a7f85b8a17b8545e0412ba161b
Marina0811 <kmario0811@gmail.com>
2021-12-23 10:15:39 +0000

    不要なフォルダの削除”

--- resources/views/weight/edit.blade.php
+++ resources/views/weight/edit.blade.php
@@ -7,7 +7,7 @@
 <div class="container">
   <div class="row">
     <div class="col-md-8 mx-auto">
-      <h2>体重登録</h2>
+      <h2>体重登録　Carbon::now()</h2>
      
       
       <form action="{{ action('WeightController@create') }}" method="post" enctype="multipart/form-data">
@@ -20,14 +20,7 @@
           </ul> 
         @endif
           
-        <div class="form-group row"> 
-          <label class="col-md-2">今日の体重は？</label>
-            <div class="col-md-10"> 
-              <input type="text" class="form-control" name="today_weight" value="{{ old('today_weight') }}">
-            </div>
-        </div>
-        {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="登録">
+        
         
         </form>
       </div>
